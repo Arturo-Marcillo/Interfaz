f860c7fa524f2cc3af8732b569d17e9d9a150e84
Arturo Adrian Marcillo Yepez <arthurlobenzo@gmail.com>
2024-01-30 09:47:43 -0500

    ACTUALIZACIÓN

--- app/src/main/java/com/example/interfaz/MainActivity.java
+++ app/src/main/java/com/example/interfaz/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarNavView);
         setSupportActionBar(toolbar);
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_user);
